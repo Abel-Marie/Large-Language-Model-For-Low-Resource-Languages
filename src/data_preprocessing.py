42cb5b002c5c7b2d64625eb09b33478032b2ad85
Abel Marie <abelmarie49@gmail.com>
2025-08-10 11:06:59 +0300

    a function to load dataset

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -4,4 +4,13 @@ import re
 import glob
 from collections import Counter
 import matplotlib.pyplot as plt
+import os
+from srs.utils_text import clean_amharic_text
+
+
+
+def load_dataset(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        lines = [line.strip() for line in f if line.strip()]
+    return pd.DataFrame({'text': lines})
 
